05fc63f254f17c12375c36528f35220b57288e6f
sebfranca <sebastien.franca@epfl.ch>
2021-11-28 13:13:20 +0100

    Working algorithm for finding all possible paths (visibility graph)

--- Global_navigation/global_path.py
+++ Global_navigation/global_path.py
@@ -1,134 +1,122 @@
 import math
-from itertools import combinations
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+import check_intersection #credits: Ansh Riyal, https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/ (cons. 27.11.2021)
 
 
 
-#test :
 
-#inputs data and useful data to have
+def find_all_paths(list_vertices, start, goal):
+    #Initialisation
+    
+    #transform list_vertices into a list of all points including start & goal. Store them without any distinction between obstacles/start/goal
+    #every entry is an instance of class Point (see check_intersection.py)
+    list_points = []         
+    for obstacle in range(len(list_vertices)):
+        for vertex in range(len(list_vertices[obstacle])):
+            x = list_vertices[obstacle][vertex][0]
+            y = list_vertices[obstacle][vertex][1]
+            list_points.append(check_intersection.Point(x,y))                   #insert obstacle vertices
+    N = len(list_points)                                                        #number of obstacle vertices
+    list_points.insert(0, check_intersection.Point(start[0],start[1]))          #insert start
+    list_points.append(check_intersection.Point(goal[0],goal[1]))               #insert goal
+    
+    #extract list_sides, the list of all obstacle sides e.g. (vertex1,vertex2),(vertex2,vertex3),etc
+    #must be grouped by obstacle list_sides = [[obstacle1.sides], [obstacle2.sides], ...]
+    #step 1: obtain the number of sides of each obstacle
+    length_obstacles = []
+    for obstacle in list_vertices:
+        length_obstacles.append(len(obstacle))
+        
+    #step 2: convert indexing of vertices from [0][0],[0][1],... into 1,2,3,...,N (0 and N+1 are start and goal respectively)
+    #then enter every tuple (side.vertex1, side.vertex2) into sides_obstacle
+    #then enter sides_obstacle into list_sides
+    list_sides = []
+    for obstacle in range(len(list_vertices)):
+        sides_obstacle = []                             #list of sides of current obstacle
+        
+        shift = sum(length_obstacles[0:obstacle]) + 1   #in order to convert indexing
+        nb_sides = length_obstacles[obstacle]
+        for vertex in range(nb_sides):
+            side = (shift + vertex,\
+                    shift + ((vertex+1) % nb_sides)) #use of % to count 1,2,3,1 instead of 1,2,3,4
+            sides_obstacle.append(side)
+        
+        list_sides.append(sides_obstacle)               #total list of sides, grouped by obstacle
+        
+    #-----------------------------------
+    #Search algorithm
+    # 4 possibilities:
+        # 1. (point1,point2) is a side of the obstacle --> valid
+        # 2. both points lie on the obstacle, but (point1,point2) is not a side of the obstacle --> not valid
+        # 3. 1st & 2nd condition false, and at least one of the points is equal to a vertex being tested --> valid
+        # 4. other conditions false, and (point1,point2) intersects (vertex1, vertex2) --> not valid
+    
+    dist = lambda x1,y1,x2,y2: math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    
+    list_neighbours = []                                                        #list of neighbours of all points
+    for point1_name in range(N+2):
+        ngb_current_point = []                                                  #list of neighbours of the current point
+        
+        for point2_name in range(N+2):
+            if point1_name==point2_name:
+                pass
+            else:
+                point1 = list_points[point1_name]
+                point2 = list_points[point2_name]
+                valid_connection = True
+                for obstacle in list_sides:
+                    obstacle_flattened = [item for sublist in obstacle for item in sublist]
+                    if ((point1_name,point2_name) in obstacle) or ((point2_name,point1_name) in obstacle): #possibility 1
+                        pass
+                    elif (point1_name in obstacle_flattened) and (point2_name in obstacle_flattened): #possibility 2
+                        valid_connection = False
+                    else:
+                        for side_vertices in obstacle:
+                            vertex1_name = side_vertices[0]
+                            vertex2_name = side_vertices[1]
+                            vertex1 = list_points[vertex1_name]
+                            vertex2 = list_points[vertex2_name]
+                            
+                            if (point1_name==vertex1_name) or (point2_name==vertex2_name)\
+                                or (point2_name==vertex1_name) or (point1_name==vertex2_name):    # possibility 3
+                                    pass
+                            elif check_intersection.doIntersect(point1, point2, vertex1, vertex2): #possibility 4
+                                    valid_connection = False
+                               
+                if valid_connection:
+                    ngb_current_point.append((point2_name, dist(point1.x, point1.y, point2.x, point2.y)))
+        
+        list_neighbours.append(ngb_current_point)
+        
+    return list_neighbours
 
-#obstacle1
-Sommet1 = (1, 1)
-Sommet2 = (10, 1)
-Sommet3 = (1, 2)
-#obstacle2
-Sommet4 = (10, 2)
-Sommet5 = (10, 0)
-Sommet6 = (0, 10)
-Sommet7 = (0, 0)
-Sommet8 = (10, 10)
-Sommet9 = (10, 10)
 
-#liste des obstacles avant agrandissement
-list_vertices = [[Sommet1, Sommet2, Sommet3],[Sommet3, Sommet4, Sommet5, Sommet6, Sommet7, Sommet8, Sommet9]]
 
-#liste des obstacles après agrandissement
-list_shifted_vertices = list_vertices*0,1
-
-#liste des cotés des obstacles = matrice B
-#--> comment faire à part le faire à la main ?
-list_obstacles_sides = [[Sommet1]]
-
+#test
+list_vertices = [[(10,10),(20,10),(15,18.66)],\
+                 [(30,25),(40,15),(53.66,18.66),(57.32,32.32),(47.32,42.32),(33.66,38.66)]]
+start = (0,0)
+goal = (70,60)
+res = find_all_paths(list_vertices, start, goal)
+"""
 #liste de tous les chemins possibles pour passer d'un sommet à l'autre = matrice A
-list_all_path = [i for i in combinations(list_vertices, 2)]
-print("all paths: {}".format(list_all_path))
-for k in list_vertices:
-    for vertex in k:
-        print(vertex)
-        sublist = [i for i in list_vertices if i != vertex]
-        print(sublist)
-
+list_all_path = [i for i in combinations(list_shifted_vertices, 2)]
 #--> ça va nous donner quoi, une liste, un array, une matrice ??
 
-"""
-# Given three collinear points p, q, r, the function checks if
-# point q lies on line segment 'pr'
-def onSegment(p, q, r):
-    if ((q.x <= max(p.x, r.x)) and (q.x >= min(p.x, r.x)) and
-            (q.y <= max(p.y, r.y)) and (q.y >= min(p.y, r.y))):
-        return True
-    return False
-
-
-def orientation(p, q, r):
-    # to find the orientation of an ordered triplet (p,q,r)
-    # function returns the following values:
-    # 0 : Collinear points
-    # 1 : Clockwise points
-    # 2 : Counterclockwise
-
-    # See https://www.geeksforgeeks.org/orientation-3-ordered-points/amp/
-    # for details of below formula.
-
-    val = (float(q.y - p.y) * (r.x - q.x)) - (float(q.x - p.x) * (r.y - q.y))
-    if (val > 0):
-
-        # Clockwise orientation
-        return 1
-    elif (val < 0):
-
-        # Counterclockwise orientation
-        return 2
-    else:
-
-        # Collinear orientation
-        return 0
-
-
-# The main function that returns true if
-# the line segment 'p1q1' and 'p2q2' intersect.
-def doIntersect(p1, q1, p2, q2):
-    # Find the 4 orientations required for
-    # the general and special cases
-    o1 = orientation(p1, q1, p2)
-    o2 = orientation(p1, q1, q2)
-    o3 = orientation(p2, q2, p1)
-    o4 = orientation(p2, q2, q1)
-
-    # General case
-    if ((o1 != o2) and (o3 != o4)):
-        return True
-
-    # Special Cases
-
-    # p1 , q1 and p2 are collinear and p2 lies on segment p1q1
-    if ((o1 == 0) and onSegment(p1, p2, q1)):
-        return True
-
-    # p1 , q1 and q2 are collinear and q2 lies on segment p1q1
-    if ((o2 == 0) and onSegment(p1, q2, q1)):
-        return True
-
-    # p2 , q2 and p1 are collinear and p1 lies on segment p2q2
-    if ((o3 == 0) and onSegment(p2, p1, q2)):
-        return True
-
-    # p2 , q2 and q1 are collinear and q1 lies on segment p2q2
-    if ((o4 == 0) and onSegment(p2, q1, q2)):
-        return True
-
-    # If none of the cases
-    return False
 
 #algo
 for k in list_all_path:
     #for i = 1 jusqu'à nombre obstacles
-      #  if doIntersect(je sais pas comment prendre toutes les valeurs de la matA et les comparer à celles de la matB)
+        if doIntersect(je sais pas comment prendre toutes les valeurs de la matA et les comparer à celles de la matB)
             print("Yes")
             list_possible_path[[i][k]] = list_all_path[[i][k]]
 
-       # else:
+        else:
             print("No")
             list_possible_path[[i][k]] = 666-666
 
     #list_possible_path = matrice C
 
-for i in list_possible_path:
+for i in list_possible_path
     list_distance_path = math.dist(list_possible_path[i])
     #list_distance_path = matrice D
 
@@ -165,6 +153,5 @@ for i in list_possible_path:
 # else:
 #     print("No")
 #
-# # This code is contributed by Ansh Riyal
-# #https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/?ref=lbp
 """
+
