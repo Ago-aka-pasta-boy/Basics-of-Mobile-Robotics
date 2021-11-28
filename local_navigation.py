91cba490ec2988cf49757015fa1103f2d2a3f575
Isione <isione.bonvalot@epfl.ch>
2021-11-28 18:43:21 +0100

    Local navigation code in aseba from Exercises 4

--- local_navigation.py
+++ local_navigation.py
@@ -5,46 +5,36 @@ Created on Sun Nov 28 18:25:59 2021
 @author: ision
 """
 
-speed0 = 100       # nominal speed
-speedGain = 2      # gain used with ground gradient
-obstThrL = 10      # low obstacle threshold to switch state 1->0
-obstThrH = 20      # high obstacle threshold to switch state 0->1
-obstSpeedGain = 5  # /100 (actual gain: 5/100=0.05)
+var speed0 = 100       # nominal speed
+var speedGain = 2      # gain used with ground gradient
+var obstThrL = 10      # low obstacle threshold to switch state 1->0
+var obstThrH = 20      # high obstacle threshold to switch state 0->1
+var obstSpeedGain = 5  # /100 (actual gain: 5/100=0.05)
 
-state = 1          # 0=gradient, 1=obstacle avoidance
-obst = [0,0]       # measurements from left and right prox sensors
+var state = 0          # 0=gradient, 1=obstacle avoidance
+var diffDelta          # difference between ground sensors
+var obst[2]            # measurements from left and right prox sensors
 
-timer_period[0] = 10   # 10ms sampling time
+timer.period[0] = 10   # 10ms sampling time
 
-
-@onevent 
-def timer0():
-    global prox_ground_delta, prox_horizontal, motor_left_target, motor_right_target, state, obst, obstThrH, obstThrL, obstSpeedGain, speed0, speedGain 
-    # acquisition from ground sensor for going toward the goal
-    diffDelta = prox_ground_delta[1] - prox_ground_delta[0]
-
-    # acquisition from the proximity sensors to detect obstacles
-    obst = [prox_horizontal[0], prox_horizontal[4]]
-    
-    # tdmclient does not support yet multiple and/or in if statements:
-    if state == 0: 
-        # switch from goal tracking to obst avoidance if obstacle detected
-        if (obst[0] > obstThrH):
-            state = 1
-        elif (obst[1] > obstThrH):
-            state = 1
-    elif state == 1:
-        if obst[0] < obstThrL:
-            if obst[1] < obstThrL : 
-                # switch from obst avoidance to goal tracking if obstacle got unseen
-                state = 0
-    if  state == 0 :
-        # goal tracking: turn toward the goal
-        leds_top = [0,0,0]
-        motor_left_target = speed0 - speedGain * diffDelta
-        motor_right_target = speed0 + speedGain * diffDelta
-    else:
-        leds_top = [30,30,30]
-        # obstacle avoidance: accelerate wheel near obstacle
-        motor_left_target = speed0 + obstSpeedGain * (obst[0] // 100)
-        motor_right_target = speed0 + obstSpeedGain * (obst[1] // 100)
+onevent timer0
+  # acquisition from ground sensor for going toward the goal
+  diffDelta = prox.ground.delta[1] - prox.ground.delta[0]
+  # acquisition from the proximity sensors to detect obstacles
+  obst = [prox.horizontal[0], prox.horizontal[4]]
+  if state == 0 and (obst[0] > obstThrH or obst[1] > obstThrH) then
+    # switch from goal tracking to obst avoidance if obstacle detected
+    state = 1
+  elseif state == 1 and obst[0] < obstThrL and obst[1] < obstThrL then
+    # switch from obst avoidance to goal tracking if obstacle got unseen
+    state = 0
+  end
+  if  state == 0 then
+    # goal tracking: turn toward the goal
+    motor.left.target = speed0 - speedGain * diffDelta
+    motor.right.target = speed0 + speedGain * diffDelta
+  else
+    # obstacle avoidance: accelerate wheel near obstacle
+    motor.left.target = speed0 + obstSpeedGain * (obst[0] / 100)
+    motor.right.target = speed0 + obstSpeedGain * (obst[1] / 100)
+  end
