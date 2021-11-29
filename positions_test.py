caea7bc09bdfb995bef707ff4640846ca744d784
inalbon <malika.in-albon@epfl.ch>
2021-11-29 23:33:17 +0100

    Debug im.Show

--- positions_test.py
+++ positions_test.py
@@ -3,10 +3,11 @@ import cv2 as cv
 import positions as pos
 import matplotlib.pyplot as plt
 
-img = cv.imread('img_test.png', cv.IMREAD_UNCHANGED).astype('float32')
+img = cv.imread('img_test.jpg', cv.IMREAD_COLOR)
 cv.imshow("image", img)
+cv.waitKey(0)
 
 success, goal = pos.get_goal_position(img)
 
-print("\n Succes", success, "Goal position:", goal, "\n")
+print("\nSuccess is", success, "\nGoal position is:", goal, "\n")
 
