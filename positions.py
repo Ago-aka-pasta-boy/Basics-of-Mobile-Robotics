b6e8902f64b579260267b1b9a4b99dc1bd53309b
inalbon <malika.in-albon@epfl.ch>
2021-11-29 15:19:09 +0100

    Finish detect_circles, and find goal and robot positions (not tested)

---
+++ positions.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2 as cv
+
+def detect_circles(img, lower_range, upper_range):
+
+    # convert img from rgb to hsv
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    # extract colored circle in white
+    mask = cv.inRange(hsv, lower_range, upper_range)
+
+    # detect circle
+    blurred = cv.blur(mask, (3, 3))
+    circles = cv.HoughCircles(blurred, cv.HOUGH_GRADIENT, 1, 20,
+                                param1 = 50, param2 = 30, minRadius = 1, maxRadius = 50)
+
+
+    # find circles center position
+    circles = np.round(circles[0, :].astype("int"))
+    return circles
+
+# (x,y)
+def get_goal_position(img):
+    # green color in hsv: 120°, 100%, 50% or 120°, 100%, 100%
+    lower_green = np.array([36,0,0])
+    upper_green = np.array([70, 255, 255])
+
+    circle = detect_circles(img, lower_green, upper_green)
+    center = (circle[0], circle[1])
+
+    return center
+
+
+# (x,y) + angle between -pi and pi
+def get_robot_position(img):
+    # blue color in hsv:
+    lower_blue = np.array([100, 50, 38])
+    upper_blue = np.array([110, 255, 255])
+
+    circles = detect_circles(img, lower_blue, upper_blue)
+
+    for i in circles[0, :]:
+        list_centers.append = (i[0], i[1])
+        list_radius.append = i[2]
+
+    if list_radius[0] > list_radius[1]:
+        center1 = list_centers[0]
+        center2 = list_centers[1]
+        radius1 = list_centers[0]
+        radius2 = centers[1]
+    else:
+        center1 = centers[1]
+        center2 = centers[0]
+        radius1 = centers[1]
+        radius2 = centers[0]
+
+    angle = atan2((center2[1]-center1[1])/(center2[1]-center2[0]))
+
+    position = np.array([center1, angle])
+
+    return position
+
+def get_arch_positions(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    lower_range = np.array([169, 100, 100])
+    upper_range = np.array([189, 255, 255])
+
+    # extract red arch in white
+    mask = cv.inRange(hsv, lower_range, upper_range)
+    blurred = cv.blur(mask, (3, 3))
+
+
+
