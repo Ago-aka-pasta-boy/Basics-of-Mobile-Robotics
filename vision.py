e8c924de7d2531c9ea5e27cc31fbc1c444199d74
Sylvain Pellegrini <sylvain.pellegrini@epfl.ch>
2021-11-30 10:51:59 +0100

    all functions should be working, might need a cleanup

--- vision.py
+++ vision.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2 as cv
 
-SCALING_FACTOR = 2
+SCALING_FACTOR = 1.2
 
 
 def crop_map(img):
     """crop the black borders out of the image to have a clean image to work on"""
     max_crop = 100
-    thresh = 200
+    thresh = 100
     crop = 0
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img_blurred = cv.GaussianBlur(img_gray, (5, 5), cv.BORDER_DEFAULT)
@@ -16,7 +16,8 @@ def crop_map(img):
         crop = i                                                                    # black dots
         cropped = img_filtered[i:-i, i:-i]
         ret, temp = cv.threshold(cropped, thresh, 255, cv.THRESH_BINARY_INV)
-        cv.imshow("Thresholded", temp)
+        if __debug__:
+            cv.imshow("Thresholded", temp)
         if np.sum(temp[1, :]) < 10 or np.sum(temp[temp.shape[0]-1, :]) < 10 or\
                 np.sum(temp[:, 1]) < 10 or np.sum(temp[:, temp.shape[1]-1]) < 10:
             break
@@ -30,38 +31,48 @@ def extract_obstacles(img):
     # cv.imshow("second channel", img[:, :, 1])
     # cv.imshow("third channel", img[:, :, 2])
     # RGB input!!
-    epsilon = 0.02
-    count = []
     thresh = 100
     img_green = img[:, :, 1]
     img_blurred = cv.GaussianBlur(img_green, (5, 5), cv.BORDER_DEFAULT)
     img_filtered = cv.morphologyEx(img_blurred, cv.MORPH_OPEN, np.ones((5, 5)))
     ret, img_ready = cv.threshold(img_filtered, thresh, 255, cv.THRESH_BINARY)
-    contours, hierarchy = cv.findContours(img_ready, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.imshow("Thresholded", img_ready)
+    contours, hierarchy = cv.findContours(img_ready, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     im2 = np.zeros(img.shape)
-    for count in contours:
-        epsilon = 0.01 * cv.arcLength(count, True)
-    approximations = cv.approxPolyDP(count, epsilon, closed=True)
-    cv.drawContours(im2, [approximations], 0, (0, 255, 0), 3)
-    print(approximations[0][0][0])
-    # cv.imshow("Contours", im2)
+    approximations = []
+    for count in range(len(contours)):
+        epsilon = 0.03 * cv.arcLength(contours[count], True)
+        if (cv.arcLength(contours[count], True) < 1000) & (cv.arcLength(contours[count], True) > 200):
+            approximations.append(cv.approxPolyDP(contours[count], epsilon, closed=True))
+    cv.drawContours(im2, approximations, -1, (0, 255, 0), 3)
+    cv.imshow("Contours", im2)
     obstacles = []
-    for i in approximations:
-        if len(approximations) < 10:
+    for i in range(len(approximations)):
+        print("approx", i, len(approximations[i]))
+        if len(approximations[i]) < 5:
             obstacles.append(approximations[i])
+    im3 = np.zeros(img.shape)
+    cv.drawContours(im3, obstacles, -1, (0, 255, 0), 3)
+    cv.imshow("obstacles", im3)
+    if not obstacles:
+        return False, []
     return True, obstacles
 
 
 def expand_obstacles(obstacles):
     """expand the size of the obstacles to take the robot size in account for the path"""
     ex_obstacles = np.copy(obstacles)
-    for c in obstacles:
-        moments = cv.moments(c)
+    for c in range(len(obstacles)):
+        moments = cv.moments(obstacles[c])
+        if moments["m00"] == 0:
+            moments["m00"] = 0.01
+        if moments["m00"] == 0:
+            moments["m00"] = 0.01
         cx = int(moments["m10"] / moments["m00"])
         cy = int(moments["m01"] / moments["m00"])
-        for d in obstacles[c]:
-            vec_x = obstacles[c][d][0]-cx
-            vec_y = obstacles[c][d][1]-cy
-            ex_obstacles[c][d][0] = cx + vec_x + SCALING_FACTOR * -1 * (obstacles[c][d][0] > cx)
-            ex_obstacles[c][d][1] = cy + vec_y + SCALING_FACTOR * -1 * (obstacles[c][d][1] > cy)
+        for d in range(len(obstacles[c])):
+            vec_x = obstacles[c][d][0][0]-cx
+            vec_y = obstacles[c][d][0][1]-cy
+            ex_obstacles[c][d][0][0] = cx + vec_x * SCALING_FACTOR
+            ex_obstacles[c][d][0][1] = cy + vec_y * SCALING_FACTOR
     return ex_obstacles
--- vision_test.py
+++ vision_test.py
@@ -4,24 +4,33 @@ import vision as vis
 import time
 
 # get img from the webcam
-cap_size = (1200, 1000)
-map_shape = [1200,  800]
-cap = cv.VideoCapture(1)                                    # put 1 if an external webcam is used
-cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_size[0])               # width
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_size[1])              # height
+#cap_size = (2500, 1500)
+#map_shape = [1200,  800]
+cap = cv.VideoCapture(0)                                    # put 2 if an external webcam is used
+#cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_size[0])               # width
+#cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_size[1])              # height
 cap.set(cv.CAP_PROP_AUTO_EXPOSURE, 1)                       # set auto exposure
 
 while True:
+    if __debug__:
+        print("debugging ON")
+    else:
+        print("debugging OFF")
     time.sleep(1)
     captured, img = cap.read()
     if captured:
         cropped, cropped_img = vis.crop_map(img)
-        cv.imshow("Original", img)
+        print(cropped_img.shape)
+        if __debug__:
+            cv.imshow("Original", img)
         cv.imshow("Cropped", cropped_img)
         if cropped:
-            found, obstacles = vis.extract_obstacles(img)
+            found, obstacles = vis.extract_obstacles(cropped_img)
             if found:
                 ex_obstacles = vis.expand_obstacles(obstacles)
+                im3 = np.zeros(cropped_img.shape)
+                cv.drawContours(im3, ex_obstacles, -1, (0, 255, 0), 3)
+                cv.imshow("expanded", im3)
     else:
         print("There was a problem in the capture")
         break
