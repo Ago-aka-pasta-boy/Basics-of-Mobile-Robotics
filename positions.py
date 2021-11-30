21a93edd517cd162f451ea4d10156138d8089f61
inalbon <malika.in-albon@epfl.ch>
2021-11-30 01:05:34 +0100

    Calibration of houghcircle and inrange done for blue and green

--- positions.py
+++ positions.py
@@ -7,40 +7,83 @@ def detect_circles(img, lower_range, upper_range):
     # convert img from rgb to hsv
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    # extract colored circles in white
+    # threshold colored circles
     mask = cv.inRange(hsv, lower_range, upper_range)
     mask_inv = cv.bitwise_not(mask)
     cv.imshow("image mask", mask)
     cv.waitKey(0)
+    cv.destroyAllWindows()
 
     # convert img from rgb to grayscale
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow("image grayscale", gray)
     cv.waitKey(0)
+    cv.destroyAllWindows()
 
-    # apply mask to grayscale image
-    img_masked = cv.bitwise_or(gray, gray, mask=mask_inv)
+    # apply threshold mask to grayscale image
+    img_masked = cv.bitwise_and(gray, gray, mask=mask)
     cv.imshow("image masked", img_masked)
     cv.waitKey(0)
+    cv.destroyAllWindows()
 
-    blurred = cv.blur(img_masked, (3, 3))
+    blurred = cv.blur(img_masked, (5, 5))
     cv.imshow("image blurred", blurred)
     cv.waitKey(0)
+    cv.destroyAllWindows()
 
     # detect circles
     circles = cv.HoughCircles(blurred, cv.HOUGH_GRADIENT, 1, 20,
-                                param1=40, param2=40, minRadius=10, maxRadius=100)
+                                param1=50, param2=30, minRadius=5, maxRadius=100)
+    if circles is None:
+        print("no circle found")
+        return None
 
-    circles = np.uint16(np.around(circles))
+    # draw circles
+    radius_map = {}
+    for n in range(20, 250, 1):
+        radius_map[n] = []
+
+    # convert the (x, y) coordinates and radius of the circles to integers
+    circles = np.round(circles[0, :]).astype("int")
+
+    """"
+    output = img.copy
+    
+    for (x, y, r) in circles:
+        radius_map[r].append((x, y, r))
+        
+    for x, y, r in circles[0,:]:
+        cv.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv.imshow("circles in green", output)
+    """
+    """
+    for (x, y, r) in circles:
+        radius_map[r].append((x, y, r))
+    
+    for key in radius_map:
+        if len(radius_map[key]) > 0:
+            output = img.copy()
+            for x, y, r in radius_map[key]:
+                cv.circle(output, (x, y), r, (0, 255, 0), 4)
+                cv.imshow(f"Radius {key}", output)
+    """
+
+    for i in circles:
+        # draw the outer circle
+        cv.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
     return circles
 
-# (x,y)
-
-
+# return position of the goal (x,y)
 def get_goal_position(img):
     # green color in hsv: 120°, 100%, 50% or 120°, 100%, 100%
-    lower_green = np.array([36, 0, 0])
-    upper_green = np.array([70, 255, 255])
+    lower_green = np.array([35, 50, 0])
+    upper_green = np.array([75, 255, 255])
 
     circle = detect_circles(img, lower_green, upper_green)
 
@@ -49,59 +92,53 @@ def get_goal_position(img):
         return False, None
 
     dim = circle.shape
-    nb_circles = dim[1]
+    nb_circles = dim[0]
     print("\ncoordinates of circle is:", circle, "\nnumber of circles detected is:", nb_circles)
 
     if nb_circles > 1:
         print("number of circles found", nb_circles)
         return False, None
 
-    center = circle[0, 0, 0:2]
+    center = circle[0, 0:2]
     return True, center
 
 
-# (x,y) + angle between -pi and pi
+# return robot position (x,y) + angle between -pi and pi
 def get_robot_position(img):
-    # blue color in hsv:
-    lower_blue = np.array([100, 50, 38])
-    upper_blue = np.array([110, 255, 255])
+    # mask for blue color in hsv:
+    lower_blue = np.array([85, 50, 0])
+    upper_blue = np.array([130, 255, 255])
 
     circles = detect_circles(img, lower_blue, upper_blue)
 
-    list_centers = []
-    list_radius = []
-
-    #for i in circles[0, :]:
-    #    list_centers.append = (i[0], i[1])
-    #    list_radius.append = i[2]
-
-    if not len(circles):
+    if circles is None:
         print("no circle found")
-        return 0
+        return False, None
 
-    elif len(circles) > 2:
-        print("more than two circles found")
-        return len(circles)
+    dim = circles.shape
+    nb_circles = dim[0]
+    print("\ncoordinates of circle is:", circles, "\nnumber of circles detected is:", nb_circles)
 
-    #if list_radius[0] > list_radius[1]:
-    #    center1 = list_centers[0]
-    #    center2 = list_centers[1]
-    #else:
-    #    center1 = list_centers[1]
-    #    center2 = list_centers[0]
+    if nb_circles > 2:
+        print("number of circles found", nb_circles)
+        return False, None
 
-    if circles[0][2] > circles[1][2]:
-        center1 = circles[0][0:1]
-        center2 = circles[1][0:1]
-    else:
-        center1 = circles[1][0:1]
-        center2 = circles[0][0:1]
+    # find bigger circle between the two
+    if nb_circles == 2:
+        if circles[0, 2] > circles[1, 2]:
+            center1 = circles[0, 0:2]
+            center2 = circles[1, 0:2]
+        else:
+            center1 = circles[1, 0:2]
+            center2 = circles[0, 0:2]
+        print("center 1 and 2", center1, center2)
 
+    # find direction of the robot
     angle = math.atan2((center2[1]-center1[1])/(center2[1]-center2[0]))
 
     position = np.array([center1, angle])
 
-    return position
+    return True, position
 
 
 def get_arch_positions(img):
--- positions_test.py
+++ positions_test.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2 as cv
 import positions as pos
-import matplotlib.pyplot as plt
 
 img = cv.imread('img_test.jpg', cv.IMREAD_COLOR)
 cv.imshow("image", img)
 cv.waitKey(0)
 
 success, goal = pos.get_goal_position(img)
-
 print("\nSuccess is", success, "\nGoal position is:", goal, "\n")
+
+
+success, robot_position = pos.get_robot_position(img)
+print("\nSuccess is", success, "\nRobot position is:", robot_position, "\n")
 
