be4fff7e438d6230bacbc632d35e752af4399d81
inalbon <malika.in-albon@epfl.ch>
2021-12-07 17:36:17 +0100

    Coded conversion pixel to m

--- positions_test.py
+++ positions_test.py
@@ -6,8 +6,11 @@ img = cv.imread('img_test_final.jpg', cv.IMREAD_COLOR)
 cv.imshow("image", img)
 cv.waitKey(0)
 
-success, goal = pos.get_goal_position(img)
-print("\nSuccess is", success, "\nGoal position is:", goal, "\n")
+success, goal, radius = pos.get_goal_position(img)
+print("\nSuccess is", success, "\nGoal position is:", goal, "\nradius", radius)
+
+factor = pos.convert_meter2pxl(radius)
+print("\nConversion factor meter to pixel", factor)
 
 
 success, robot_position = pos.get_robot_position(img)
