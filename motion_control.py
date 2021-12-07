ccc167c06e60ff5603669b7c5b19fd2187012a75
sebfranca <sebastien.franca@epfl.ch>
2021-12-07 15:24:57 +0100

    On debuggue

--- motion_control.py
+++ motion_control.py
@@ -1,23 +1,25 @@
 import math
 
-KP = 150
+KP = 75
 EPSILON = 0.1  # 15 pixels
 NORMAL_SPEED = 150
 MAX_DIV = 1
 
-POSITION_ERROR = 5
+POSITION_ERROR = 20
 
 
 def speed_control(err):
     if err < -EPSILON:
         # turn right
-        motor_left = NORMAL_SPEED/(max(abs(err),MAX_DIV)) + KP*err
-        motor_right = NORMAL_SPEED/(max(abs(err),MAX_DIV)) - KP*err
-
-    elif err > EPSILON:
-        # turn left
-        motor_left = NORMAL_SPEED/(max(abs(err),MAX_DIV)) - KP * err
-        motor_right = NORMAL_SPEED/(max(abs(err),MAX_DIV)) + KP * err
+        motor_left = NORMAL_SPEED + KP*abs(err)
+        motor_right = -(NORMAL_SPEED + KP*abs(err))
+        print("first if")
+#/(max(abs(err),MAX_DIV))
+    elif err > EPSILON: 
+    # turn left     
+        motor_left =-( NORMAL_SPEED + KP * abs(err))
+        motor_right = NORMAL_SPEED + KP * abs(err)
+        print("second if")
 
     else:
         # go straight
